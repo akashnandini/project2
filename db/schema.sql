d96cc43a37d5bc7f6ac5c104fb69b40e58698a80
Minal Karani <karani.minal@gmail.com>
2019-03-05 19:12:26 -0500

    MVCstructure

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,27 @@
-CREATE DATABASE bet_db;
-USE bet_db;
+CREATE DATABASE bet;
+USE bet;
 
 CREATE TABLE users
 (
-	id int NOT NULL AUTO_INCREMENT,
-	user_name varchar(255) NOT NULL,
-	email_id varchar(255) NOT NULL,
-	password varchar(255) NOT NULL,
-	PRIMARY KEY (id)
+    id int NOT NULL AUTO_INCREMENT,
+    user_name varchar(255) NOT NULL,
+    email_id varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    PRIMARY KEY (id)
 );
 CREATE TABLE category
 (
-	id integer NOT NULL AUTO_INCREMENT,
-	category_name varchar(255) NOT NULL,
-	PRIMARY KEY (id)
+    id integer NOT NULL AUTO_INCREMENT,
+    category_name varchar(255) NOT NULL,
+    PRIMARY KEY (id)
 );
 CREATE TABLE bills
 (
-	id integer NOT NULL AUTO_INCREMENT,
-	amount integer(100) NOT NULL,
+    id integer NOT NULL AUTO_INCREMENT,
+    amount float (7,2) NOT NULL,
     bill_date timestamp,
-	category varchar(255) NOT NULL,
-	spent_in integer(100) NOT NULL,
-	remarks varchar(255) NOT NULL,
-	PRIMARY KEY (id)
+    category varchar(255) NOT NULL,
+    spent_at varchar(200) NOT NULL,
+    remarks varchar(400) ,
+    PRIMARY KEY (id)
 );
